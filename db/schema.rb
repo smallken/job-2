490c611a14176a5b62906942d4ee81ac80e75055
周文耿 <412582902@qq.com>
2017-07-05 00:18:24 +0800

    implement part 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704092224) do
+ActiveRecord::Schema.define(version: 20170704160649) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
